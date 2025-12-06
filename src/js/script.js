b79b16bf936767149b5e9ee702b7360b5fda7aaa
jstzaa <fahrizakurniawan51@gmail.com>
2025-12-06 21:21:02 +0700

    add: adding tabs for skills section

--- src/js/script.js
+++ src/js/script.js
@@ -3,5 +3,25 @@ const sideButton = document.getElementById("sidebar");
 sideButton.addEventListener("click", function () {
   const navbar = document.getElementById("navbar");
 
-  navbar.classList.toggle("opacity-0");
+  navbar.classList.toggle("invisible");
 });
+
+// const tabButton = document.querySelectorAll("#btn-tab");
+// const techStack = document.getElementById("tech_stack");
+// const tools = document.getElementById("tools");
+
+// tabButton[0].addEventListener("click", function () {
+//   techStack.classList.remove("hidden");
+//   techStack.classList.add("grid");
+
+//   tools.classList.remove("grid");
+//   tools.classList.add("hidden");
+// });
+
+// tabButton[1].addEventListener("click", function () {
+//   techStack.classList.remove("grid");
+//   techStack.classList.add("hidden");
+
+//   tools.classList.remove("hidden");
+//   tools.classList.add("grid");
+// });
