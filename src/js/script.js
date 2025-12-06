b28d2f89a42e44245012951a1b00778a380f4d47
jstzaa <fahrizakurniawan51@gmail.com>
2025-12-06 13:38:23 +0700

    fix: sidebar and image directory

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
-const sideButton = document.getElementById('sidebar')
+const sideButton = document.getElementById("sidebar");
 
-sideButton.addEventListener('click', function(){
-    
-})
+sideButton.addEventListener("click", function () {
+  const navbar = document.getElementById("navbar");
+
+  navbar.classList.toggle("opacity-0");
+});
